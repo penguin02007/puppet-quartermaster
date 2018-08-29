66eb526d44666d37c452fbad3e94b0cb410e3c9b
Leo Chan <ka.chan@rakops.com>
2018-08-29 10:41:41 +0200

    remove unneeded os in minimum

--- examples/minimum.pp
+++ examples/minimum.pp
@@ -3,19 +3,8 @@ class { 'quartermaster':
     matchbox_enable      => true,
     puppetmaster         => "puppetmaster.${domain}",
   }
-  # Fedora
-  quartermaster::pxelinux{'fedora-27-x86_64':}
-  # Ubuntu
   quartermaster::pxelinux{'ubuntu-16.04-amd64':}
-  quartermaster::pxelinux{'ubuntu-16.04-i386':}
-  quartermaster::pxelinux{'ubuntu-17.10-amd64':}
-  quartermaster::pxelinux{'ubuntu-17.10-i386':}
-  quartermaster::pxelinux{'ubuntu-18.04-i386':}
   quartermaster::pxelinux{'ubuntu-18.04-amd64':}
   # Centos
   quartermaster::pxelinux{'centos-6.9-x86_64':}
   quartermaster::pxelinux{'centos-7.4.1708-x86_64':}
-  # CoreOS
-  quartermaster::pxelinux{'coreos-stable-amd64':}
-  # RancherOS
-  quartermaster::pxelinux{'rancheros-1.3.0-amd64':}
